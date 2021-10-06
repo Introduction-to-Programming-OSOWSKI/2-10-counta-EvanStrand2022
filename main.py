19b21e2d0277afaf06d16ad03d7a2438111ff90e
EvanStrand2022 <818584@mystma.org>
2021-10-06 09:31:05 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#WRITE YOUR CODE IN THIS FILE
+#define function
+def countA(w):
+    total = 0
+    for i in range(0, len(w)):
+        if w[i] == "a":
+            total = total+1
+    return total
